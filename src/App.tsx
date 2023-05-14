a6b284a6ef9157ad2ca086d4b328e49e5c107c6c
Timmydee <dahunsitimmy@gmail.com>
2023-05-14 16:38:45 +0100

    w

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className='w-full h-[100vh] lg:p-20'>
       <h1 className='font-bold text-green-700 text-2xl mb-4 text-left'>Weather Buddy</h1>
-      <div className='flex flex-col lg:row'>
+      <div className='flex flex-col lg:flex-row'>
         <div className='lg:w-[30%] border lg:h-full w-[100%] h-[30vh] '>
           <Form />
         </div>
